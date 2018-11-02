2f378ae189c3df8a1af8d1c7cf34ed07a1a5d0f3
Juliana Prieto <jprietoarcila@gmail.com>
2018-11-02 13:51:52 -0500

    Proceso para poder llenar Surtido

---
+++ superandes-iteracion2-j.prieto-s.lemus/data/dataTablas/SurtidoData.sql
@@ -0,0 +1,114 @@
+--Cuando insertes los datos no te preocupes por los errores
+-- Categoria 1
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', '7U7M63Z6N4KK3', 28);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 49);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('800', 'P50TP766F9ZD4', 11);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'IJF3GHKO3SJP8', 29);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'NKD8O17706W62', 35);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('800', '7U7M63Z6N4KK3', 17);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', '7U7M63Z6N4KK3', 24);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('800', 'NKD8O17706W62', 24);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'IJF3GHKO3SJP8', 18);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 23);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', '7U7M63Z6N4KK3', 35);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', '7U7M63Z6N4KK3', 24);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('800', 'NKD8O17706W62', 12);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'NKD8O17706W62', 38);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'E1T2DXN40I6JH', 23);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'IJF3GHKO3SJP8', 49);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'NKD8O17706W62', 24);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'IJF3GHKO3SJP8', 12);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 47);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 13);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 34);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('800', 'NKD8O17706W62', 43);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('800', '7U7M63Z6N4KK3', 36);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 27);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 18);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 37);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'NKD8O17706W62', 32);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', '7U7M63Z6N4KK3', 38);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'IJF3GHKO3SJP8', 47);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', '6XT34L0Q9X3EX', 40);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('786', 'E1T2DXN40I6JH', 37);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 16);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'IJF3GHKO3SJP8', 22);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 28);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 25);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'NKD8O17706W62', 30);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('782', 'E1T2DXN40I6JH', 15);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('800', '6XT34L0Q9X3EX', 46);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('800', 'NKD8O17706W62', 40);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 26);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 35);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', '7U7M63Z6N4KK3', 41);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'NKD8O17706W62', 31);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'P50TP766F9ZD4', 13);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'NKD8O17706W62', 37);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('778', 'NKD8O17706W62', 20);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 12);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('782', '7U7M63Z6N4KK3', 35);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 31);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 21);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 50);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'NKD8O17706W62', 37);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('800', 'NKD8O17706W62', 20);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 13);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'P50TP766F9ZD4', 32);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', '7U7M63Z6N4KK3', 34);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('800', 'NKD8O17706W62', 16);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', '7U7M63Z6N4KK3', 40);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'P50TP766F9ZD4', 47);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', '7U7M63Z6N4KK3', 20);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 22);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 49);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'NKD8O17706W62', 44);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'IJF3GHKO3SJP8', 13);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'IJF3GHKO3SJP8', 16);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'PF743A8YB1RT3', 28);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('800', 'NKD8O17706W62', 22);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 43);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'E1T2DXN40I6JH', 16);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('786', 'P50TP766F9ZD4', 17);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 20);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'IJF3GHKO3SJP8', 36);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'IJF3GHKO3SJP8', 39);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('800', '7U7M63Z6N4KK3', 12);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('800', 'P50TP766F9ZD4', 15);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 22);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'IJF3GHKO3SJP8', 33);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 42);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', '7U7M63Z6N4KK3', 15);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'NKD8O17706W62', 49);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 32);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 35);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', '7U7M63Z6N4KK3', 38);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'IJF3GHKO3SJP8', 16);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 27);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'IJF3GHKO3SJP8', 41);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('778', 'IJF3GHKO3SJP8', 38);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', '7U7M63Z6N4KK3', 44);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 19);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'IJF3GHKO3SJP8', 14);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('786', 'NKD8O17706W62', 48);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'IJF3GHKO3SJP8', 16);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('786', 'NKD8O17706W62', 11);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 17);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('803', 'NKD8O17706W62', 18);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('786', 'NKD8O17706W62', 27);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('778', '7U7M63Z6N4KK3', 17);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'NKD8O17706W62', 24);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', '7U7M63Z6N4KK3', 24);
+insert into SURTIDO (id_estante, id_producto, cantidad) values ('807', 'IJF3GHKO3SJP8', 39);
+
+-- Categoria 2
+-- Categoria 3
+-- Categoria 4
+-- Categoria 5
+-- Categoria 6
+-- Categoria 7
+-- Categoria 8
+-- Categoria 9
+
+
+COMMIT
